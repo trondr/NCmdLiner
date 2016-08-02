c13a326156edbd50d7ab6ca6a9b54ddcbbd348da
trondr <trondr@outlook.com>
2016-08-02 03:36:03 +0200

    Started developing build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,38 @@
 @Echo Off
 Set ProductName=NCmdLiner
-dotnet build "%~dp0src\%ProductName%"
-dotnet build "%~dp0test\%ProductName%.Tests"
-dotnet test "%~dp0test\%ProductName%.Tests"
+dotnet restore "%~dp0src\%ProductName%"
+
+Set Configuration=Release
+
+Set DotNetFrameWork=net35
+Set OutputDir=%DotNetFrameWork%
+dotnet build "%~dp0src\%ProductName%" --output "%~dp0bin\%Configuration%\%OutputDir%" --build-base-path "%~dp0bin\obj\%Configuration%\%OutputDir%" --configuration Release --framework %DotNetFrameWork%
+
+Set DotNetFrameWork=net452
+Set OutputDir=%DotNetFrameWork%
+dotnet build "%~dp0src\%ProductName%" --output "%~dp0bin\%Configuration%\%OutputDir%" --build-base-path "%~dp0bin\obj\%Configuration%\%OutputDir%" --configuration Release --framework %DotNetFrameWork%
+
+Set DotNetFrameWork=net461
+Set OutputDir=%DotNetFrameWork%
+dotnet build "%~dp0src\%ProductName%" --output "%~dp0bin\%Configuration%\%OutputDir%" --build-base-path "%~dp0bin\obj\%Configuration%\%OutputDir%" --configuration Release --framework %DotNetFrameWork%
+
+Set DotNetFrameWork=.NETFramework,Version=v4.5,Profile=Mono
+Set OutputDir=net45-mono
+dotnet build "%~dp0src\%ProductName%" --output "%~dp0bin\%Configuration%\%OutputDir%" --build-base-path "%~dp0bin\obj\%Configuration%\%OutputDir%" --configuration Release --framework %DotNetFrameWork%
+
+Set DotNetFrameWork=netstandard1.6
+Set OutputDir=%DotNetFrameWork%
+dotnet build "%~dp0src\%ProductName%" --output "%~dp0bin\%Configuration%\%OutputDir%" --build-base-path "%~dp0bin\obj\%Configuration%\%OutputDir%" --configuration Release --framework %DotNetFrameWork%
+
+dotnet restore "%~dp0test\%ProductName%.Tests"
+Set DotNetFrameWork=netcoreapp1.0
+Set OutputDir=%DotNetFrameWork%
+dotnet build "%~dp0test\%ProductName%.Tests" --output "%~dp0bin\%Configuration%\Tests\%OutputDir%" --build-base-path "%~dp0bin\obj\%Configuration%\Tests\%OutputDir%" --configuration Release --framework %DotNetFrameWork%
+dotnet test "%~dp0test\%ProductName%.Tests" --output "%~dp0bin\%Configuration%\Tests\%OutputDir%" --build-base-path "%~dp0bin\obj\%Configuration%\Tests\%OutputDir%" --configuration Release --framework %DotNetFrameWork%
+
+Set DotNetFrameWork=net461
+Set OutputDir=%DotNetFrameWork%
+dotnet build "%~dp0test\%ProductName%.Tests" --output "%~dp0bin\%Configuration%\Tests\%OutputDir%" --build-base-path "%~dp0bin\obj\%Configuration%\Tests\%OutputDir%" --configuration Release --framework %DotNetFrameWork%
+dotnet test "%~dp0test\%ProductName%.Tests" --output "%~dp0bin\%Configuration%\Tests\%OutputDir%" --build-base-path "%~dp0bin\obj\%Configuration%\Tests\%OutputDir%" --configuration Release --framework %DotNetFrameWork%
+
+dotnet pack "%~dp0src\%ProductName%" --output "%~dp0bin\NugetPackages" --build-base-path "%~dp0bin\obj\%Configuration% --configuration Release
