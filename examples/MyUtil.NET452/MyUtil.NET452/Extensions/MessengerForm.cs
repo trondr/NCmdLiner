c96ae17876f74caf94adde701e77cc77c7e5b7ea
trondr <trondr@outlook.com>
2016-08-04 00:26:44 +0200

    Adding NET452 version of the MyUtil example console application

---
+++ examples/MyUtil.NET452/MyUtil.NET452/Extensions/MessengerForm.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace MyUtil.Extensions
+{
+    public partial class MessengerForm : Form
+    {
+        public MessengerForm(string message)
+        {            
+            InitializeComponent();
+            _messageRichTextBox.Text = message;            
+        }
+    }
+}
