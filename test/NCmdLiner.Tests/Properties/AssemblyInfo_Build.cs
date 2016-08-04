d0a4842eecb93ab9235e3e519b50369c9f4c89b6
trondr <trondr@outlook.com>
2016-08-04 17:14:37 +0200

    NCmdLiner.1.2.16217.176

--- test/NCmdLiner.Tests/Properties/AssemblyInfo_Build.cs
+++ test/NCmdLiner.Tests/Properties/AssemblyInfo_Build.cs
@@ -8,13 +8,13 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyDescription("NCmdLiner.Tests contains tests of the NCmdLiner library.")]
+[assembly: System.Reflection.AssemblyDescription("NCmdLiner.Tests features NCmdLiner tests.")]
 [assembly: System.Reflection.AssemblyCompany("github.com/trondr")]
 [assembly: System.Reflection.AssemblyProduct("NCmdLiner.Tests")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright © <github.com/trondr> 2016")]
 [assembly: System.CLSCompliant(false)]
-[assembly: System.Reflection.AssemblyInformationalVersion("1.2.16215.169.28fbdf0")]
-[assembly: System.Reflection.AssemblyVersion("1.2.16215.169")]
-[assembly: System.Reflection.AssemblyFileVersion("1.2.16215.169")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.2.16217.176.fefd8a7")]
+[assembly: System.Reflection.AssemblyVersion("1.2.16217.176")]
+[assembly: System.Reflection.AssemblyFileVersion("1.2.16217.176")]
 
 
