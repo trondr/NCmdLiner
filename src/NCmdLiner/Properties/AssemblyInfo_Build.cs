3cc2335f08cbefe1ba8d30c484d8dbe02430d537
trondr <trondr@outlook.com>
2017-03-28 20:44:47 +0200

    NCmdLiner.1.2.17086.227

--- src/NCmdLiner/Properties/AssemblyInfo_Build.cs
+++ src/NCmdLiner/Properties/AssemblyInfo_Build.cs
@@ -11,10 +11,10 @@
 [assembly: System.Reflection.AssemblyDescription("NCmdLiner features a command line parser with auto documentation capabilities.")]
 [assembly: System.Reflection.AssemblyCompany("github.com/trondr")]
 [assembly: System.Reflection.AssemblyProduct("NCmdLiner")]
-[assembly: System.Reflection.AssemblyCopyright("Copyright © <github.com/trondr> 2016")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright © <github.com/trondr> 2017")]
 [assembly: System.CLSCompliant(false)]
-[assembly: System.Reflection.AssemblyInformationalVersion("1.2.16218.181.ca42e61")]
-[assembly: System.Reflection.AssemblyVersion("1.2.16218.181")]
-[assembly: System.Reflection.AssemblyFileVersion("1.2.16218.181")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.2.17086.227.6e00900")]
+[assembly: System.Reflection.AssemblyVersion("1.2.17086.227")]
+[assembly: System.Reflection.AssemblyFileVersion("1.2.17086.227")]
 
 
